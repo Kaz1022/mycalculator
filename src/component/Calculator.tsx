5d0f94f153f4f0dd0108324f45a3c487a76538d5
Kazuko Baynton <kazukobaynton@gmail.com>
2023-06-15 12:18:30 -0700

    add calculate funtion files

--- src/component/Calculator.tsx
+++ src/component/Calculator.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import { State } from "../logic/calculate";
+import calculate from "../logic/calculate"
 import ButtonPanel from "./ButtonPanel";
 import Display from "./Display";
 
 export default function Calculator() {
+  const [state, setState] = useState<State>({
+    current: "0",
+    operand: 0,
+    operator: null,
+    isNextClear: false
+  });
 
   const buttonHandler = (code: string) => {
-    console.log(code);
-  }
+    const nextState = calculate(code, state);
+    setState(nextState);
+  };
   return <div>
     <Display />
     <ButtonPanel buttonHandler={buttonHandler} />
