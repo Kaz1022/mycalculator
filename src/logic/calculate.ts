0f7c298365d8db4a6eab59a0d1c86779d68f65d4
Kazuko Baynton <kazukobaynton@gmail.com>
2023-06-15 12:51:39 -0700

    add button functions

--- src/logic/calculate.ts
+++ src/logic/calculate.ts
@@ -1,4 +1,21 @@
 export default function calculate(button: string, state: State): State {
+  if (isNUmberButton(button)) {
+    return handleNumberButton(button, state);
+  }
+  if (isOperatorButton(button)) {
+    return handleOperatorButton(button, state);
+  }
+  if (isDotButton(button)) {
+  }
+  if (isDeleteButton(button)) {
+  }
+  if (isAllClearButton(button)) {
+  }
+  if (isEqualButton(button)) {
+    
+  }
+
+
   return state;
 }
 
@@ -7,4 +24,47 @@ export interface State {
   operand: number;
   operator: string | null;
   isNextClear: boolean;
+}
+
+function isNUmberButton(button: string) {
+  return !isNaN(+button); 
+}
+
+function handleNumberButton(button: string, state: State): State {
+  if (state.current === "0") {
+    return {
+      current: button,
+      operand: state.operand,
+      operator: state.operator,
+      isNextClear: false
+    }
+  }
+  return {
+    current: state.current + button,
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false
+  }
+}
+
+function isOperatorButton(button: string) {
+  return button === "+" || button === "-";
+}
+
+function handleOperatorButton(button: string, state: State): State {
+  if (state.operator === null){
+    return {
+      current: state.current,
+      operand: parseFloat(state.current),
+      operator: button,
+      isNextClear: true
+    }
+  }
+  const nextValue = operate(state);
+  return {
+    current: `${nextValue}`,
+    opperand: nextValue,
+    operator: button,
+    isNextClear: true
+  }
 }
